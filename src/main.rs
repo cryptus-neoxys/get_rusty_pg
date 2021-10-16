a4a7651df638a72d3f980d28525ee5ef7a4876f6
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-17 04:29:22 +0530

    ✨ feat: methods, related fns and traits

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+use std::fmt;
+
+#[derive(Debug)]
 struct Object {
-    width: i32,
-    height: i32,
+    width: u32,
+    height: u32,
 }
 
-fn area(obj: &Object) -> i32 {
-    obj.width * obj.height
+// Methods
+impl Object {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn show(&self) {
+        println!("{}x{} with area: {}", self.height, self.width, self.area());
+    }
+}
+
+// Related Functions
+impl Object {
+    fn new(width: u32, height: u32) -> Object {
+        Object { width, height }
+    }
+}
+
+// traits
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({} x {})", self.width, self.height,)
+    }
 }
 
 fn main() {
@@ -13,5 +37,11 @@ fn main() {
         height: 20,
     };
 
-    println!("{}x{} with area: {}", o.height, o.width, area(&o));
+    let obj = Object::new(51, 38);
+
+    o.show();
+    obj.show();
+
+    println!("{}", o);
+    println!("{}", obj);
 }
