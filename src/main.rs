932a6ab5414859b41ba7edf4dc8156186e957e6d
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-18 21:58:34 +0530

    ✨ feat: collections (vec, hm) and while let bind

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,84 @@
-fn division(x: f64, y: f64) -> Option<f64> {
-    if y == 0.0 {
-        None
-    } else {
-        Some(x / y)
-    }
+use std::collections::HashMap;
+
+#[derive(Debug)]
+enum Multi {
+    Float(f64),
+    Int(i32),
+    Text(String),
 }
 
 fn main() {
-    let res = division(50.12, 0.0);
-    match res {
-        Some(x) => println!("{:.8}", x),
-        None => println!("Cannot divide"),
+    let mut v = Vec::new();
+
+    for i in &v {
+        println!("{}", i)
+    }
+    v.push(13);
+    for i in &v {
+        println!("{}", i)
+    }
+
+    println!("{:?}", v.pop());
+    println!("v: {:?}, l: {}, cap: {}", &v, v.len(), v.capacity());
+    println!("{:?}", v.pop());
+
+    let m = vec![
+        Multi::Float(1.1),
+        Multi::Int(32),
+        Multi::Text("Scuk it".to_string()),
+    ];
+
+    println!("{:?}", &m);
+
+    let mut hm = HashMap::new();
+    hm.insert(String::from("Dev"), 10);
+    hm.insert(String::from("Nishant"), 6);
+    hm.insert(String::from("Radhika"), 4);
+
+    for (k, v) in &hm {
+        println!("{}: {}", k, v);
+    }
+    hm.remove(&String::from("Dev"));
+
+    match hm.get(&String::from("Dev")) {
+        Some(&v) => println!("{}", v),
+        _ => println!("No val found for Dev"),
+    }
+
+    let j = Some('x');
+
+    if let Some(q) = j {
+        println!("{}", q);
+    } else {
+        {}
+    }
+
+    let mut o = Some(0);
+
+    // loop {
+    //     match o {
+    //         Some(b) => {
+    //             if b > 19 {
+    //                 println!("Quit");
+    //                 o = None
+    //             } else {
+    //                 println!("{}", b);
+    //                 o = Some(b + 2);
+    //             }
+    //         }
+    //         _ => {
+    //             break;
+    //         }
+    //     }
+    // }
+
+    while let Some(i) = o {
+        if i > 19 {
+            println!("Quit");
+            o = None;
+        } else {
+            println!("{}", i);
+            o = Some(i + 2);
+        }
     }
 }
