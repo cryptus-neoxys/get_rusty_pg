4442152ed99052f0bb247fc99792bfc7b5b48976
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-18 21:13:48 +0530

    ✨ feat: enums

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,90 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum Direction {
+    Up(Point),
+    Down(Point),
+    Left(Point),
+    Right(Point),
+}
+
+#[derive(Debug)]
+enum Keys {
+    UpKey(String),
+    DownKey(String),
+    LeftKey(String),
+    RightKey(String),
+}
+
+impl Direction {
+    fn match_direction(&self) -> Keys {
+        match *self {
+            Direction::Up(_) => Keys::UpKey(String::from("pressed w")),
+            Direction::Down(_) => Keys::DownKey(String::from("pressed s")),
+            Direction::Left(_) => Keys::LeftKey(String::from("pressed a")),
+            Direction::Right(_) => Keys::RightKey(String::from("pressed d")),
+        }
+    }
+}
+
+impl Keys {
+    fn destruct(&self) -> &String {
+        match *self {
+            Keys::UpKey(ref s) => s,
+            Keys::DownKey(ref s) => s,
+            Keys::LeftKey(ref s) => s,
+            Keys::RightKey(ref s) => s,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Shape {
+    Rectangle { width: u32, height: u32 },
+    Square(u32),
+    Circle(f64),
+}
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match *self {
+            Shape::Square(ref s) => (s * s) as f64,
+            Shape::Circle(ref r) => 3.14159265 * (r * r),
+            Shape::Rectangle { width, height } => (width * height) as f64,
+        }
+    }
+}
+
 fn main() {
-    let n = 6;
+    let u = Direction::Up(Point { x: 12, y: 24 });
+    let k = u.match_direction();
+    let x = k.destruct();
 
-    let x = if n < 6 { "Test" } else { "Spec" };
-    println!("n: {}, x: {}", n, x);
+    println!("{}", x);
 
-    let mut c = 4;
-    loop {
-        println!("{}", c);
-        if c <= 0 {
-            break;
-        }
-        c -= 1;
+    let u = 10;
+    let v = &u;
+    let ref w = u;
+
+    if v == w {
+        println!("v == w");
     }
 
-    'a: loop {
-        println!("loop a");
-        'b: loop {
-            println!("loop b");
-
-            'c: loop {
-                println!("loop c");
-
-                break 'b;
-            }
-        }
-        break 'a;
-    }
-
-    // break assignment
-    let f = loop {
-        break 10;
+    let r = Shape::Rectangle {
+        width: 12,
+        height: 13,
     };
+    let c = Shape::Circle(14.0);
+    let s = Shape::Square(12);
 
-    println!("f: {}", f);
+    let rr = r.area();
+    let cr = c.area();
+    let sr = s.area();
 
-    // while
-    let mut d = 0;
-
-    while d < 2 {
-        println!("d: {}", d);
-        d += 1;
-    }
-
-    // for in
-
-    // each
-    let v = vec![10, 100, 1000];
-    for i in v {
-        println!("i: {}", i);
-    }
-    // range
-    for i in 1..=5 {
-        println!("{}", i * 10);
-    }
-
-    // match (switch on STEROIDS)
-    let m = 10;
-    // other
-    match m {
-        1 => println!("one"),
-        2 => println!("two"),
-        3 => println!("three"),
-        4 => println!("four"),
-        5 => println!("five"),
-        _ => println!("unrecognized"),
-    }
-    // rust
-    match m {
-        1 => println!("The one!"),
-        2 | 3 | 5 | 7 => println!("Prime Time"),
-        12..=20 => println!("Teen"),
-        _ => println!("meh"),
-    }
-    // more rust
-    let p = (1, -9);
-    match p {
-        (0, y) => println!("y: {}", y),
-        (x, -9) => println!("x: {}", x),
-        _ => println!("matched none"),
-    }
-    let p2 = (2, -2);
-    match p2 {
-        (x, y) if x == -y => println!("{} + {} = 0", x, y),
-        (x, y) if x == y => println!("x = y = {}", x),
-        (x, _) if x % 2 == 0 => println!("x is even: {}", x),
-        _ => println!("no match found"),
-    }
-    let n = match m {
-        n @ 1..=12 => n,
-        n @ 13..=19 => n,
-        _ => 0,
-    };
-    println!("n binds: {}", n)
+    println!("Circle: {:.4}\nSquare: {}\nRect: {}", cr, sr, rr)
 }
