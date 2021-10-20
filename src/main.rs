82a4399f0b62a932b548d52e4c3a23f98a0c8699
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-20 20:18:01 +0530

    🚧 traits

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
-use std::fs::File;
+trait Shape {
+    fn area(&self) -> u32;
+}
+
+struct Rectangle {
+    x: u32,
+    y: u32,
+}
+
+struct Circle {
+    radius: f64,
+}
+
+impl Shape for Rectangle {
+    fn area(&self) -> u32 {
+        self.x * self.y
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self) -> u32 {
+        (std::f64::consts::PI * self.radius * self.radius) as u32
+    }
+}
+
+fn fib(n: i32) -> i32 {
+    match n {
+        0..=1 => n,
+        _ => fib(n - 1) + fib(n - 2),
+    }
+}
 
 fn main() {
-    let f = File::open("test.txt");
+    let c = Circle { radius: 2.11 };
+    let r = Rectangle { x: 2, y: 30 };
 
-    let f = match f {
-        Ok(file) => file,
-        Err(err) => {
-            panic!("There was an error opening file: {:?}", err)
-        }
-    };
+    println!("{} {}", c.area(), r.area());
+
+    println!("{}", fib(10));
 }
