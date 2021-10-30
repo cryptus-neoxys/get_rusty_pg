e55b5d7aaf603cbf5d1d5f3beafb558ccfced4b5
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-30 23:43:17 +0530

    🚧 refs and lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+fn pr<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() == y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct A<'a, 'b> {
+    x: &'a str,
+    y: &'b str,
+}
+
+fn main() {
+    let x;
+    // {
+    let y = 10;
+
+    x = &y;
+    // }
+    println!("x: {}", x);
+
+    let a = "a string";
+    let b = "b string";
+
+    let c = pr(a, b);
+    println!("c: {}", c);
+
+    let ast = A {
+        x: "Hello",
+        y: "There",
+    };
+    println!("{} {}!", ast.x, ast.y)
+}
